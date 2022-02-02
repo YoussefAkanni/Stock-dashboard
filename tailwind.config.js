b99b4768321e87b9c67f3f5f3074a102aff022cb
YoussefAkanni <89466060+YoussefAkanni@users.noreply.github.com>
2022-02-02 11:59:26 +0100

    push for pc

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+    },
+  },
+  plugins: [],
+};
