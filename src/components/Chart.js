882009a7f51d3286feebc63d317d04a20396635d
YoussefAkanni <89466060+YoussefAkanni@users.noreply.github.com>
2022-02-07 09:13:23 +0100

    finish dark mode

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {
   Area,
   AreaChart,
@@ -10,10 +10,15 @@ import {
 import { mockHistoricalData } from "../constants/mock";
 import { convertUnixTimestampToDate } from "../helpers/date-helper";
 import Card from "./Card";
+import ChartFilter from "./ChartFilter";
+import { chartConfig } from "../constants/config";
+import ThemeContext from "../context/ThemeContext";
 
 const Chart = () => {
   const [data, setData] = useState(mockHistoricalData);
   const [filter, setFilter] = useState("1W");
+  const { darkMode } = useContext(ThemeContext);
+
   const formatData = () => {
     return data.c.map((item, index) => {
       return {
@@ -24,16 +29,49 @@ const Chart = () => {
   };
   return (
     <Card>
+      <ul className="flex absolute top-2 right-2 z-40">
+        {Object.keys(chartConfig).map((item) => {
+          return (
+            <li key={item}>
+              <ChartFilter
+                text={item}
+                active={filter === item}
+                onClick={() => {
+                  setFilter(item);
+                }}
+              />
+            </li>
+          );
+        })}
+      </ul>
       <ResponsiveContainer>
         <AreaChart data={formatData(data)}>
+          <defs>
+            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
           <Area
             type={"monotone"}
             dataKey="value"
             stroke="#312e81"
             fillOpacity={1}
             strokeWidth={0.5}
+            fill="url(#chartColor)"
           />
-          <Tooltip />
+          <Tooltip
+            contentStyle={darkMode ? { backgroundColor: "111827" } : null}
+            itemStyle={darkMode ? { Color: "#818cf8" } : null}
+          />
           <XAxis dataKey={"date"} />
           <YAxis domain={["dataMin", "dataMax"]} />
         </AreaChart>
